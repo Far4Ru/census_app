26c3094aacd5be8e58d7e32423c5f09f848727a2
Far4Ru <faty422@mail.ru>
2023-03-03 13:07:08 +0300

    Добавлено изображение приложения

--- templates/layout/app.php
+++ templates/layout/app.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html lang="ru">
 <head>
-  <title>CakePHP 4 CRUD Using Ajax</title>
+  <title>Перепись</title>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
